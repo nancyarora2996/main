46d8f3db20379402e570b1f1f99a80c2977d09ca
nancyarora2996 <nancyarora2996@gmail.com>
2025-02-16 17:39:12 -0500

    updates

--- valet-api-tests/cypress.config.ts
+++ valet-api-tests/cypress.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://www.bankofcanada.ca/valet',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      // Add the Mochawesome reporter plugin
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    }
+    
   },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Valet API Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  }
 });
--- valet-api-tests/cypress/cypress.config.ts
+++ valet-api-tests/cypress/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'https://www.bankofcanada.ca/valet',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // Add the Mochawesome reporter plugin
       require('cypress-mochawesome-reporter/plugin')(on);
